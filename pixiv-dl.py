05ebf77d0e17e4c59a6f768208bd036e71b32109
RC-14 <61058098+RC-14@users.noreply.github.com>
2024-06-05 22:41:22 +0200

    Allow specifying an output directory

--- pixiv-dl.py
+++ pixiv-dl.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+from genericpath import isdir, isfile
 from sys import argv
 import requests
 
 def print_help():
-	print(f'Usage: {argv[0][argv[0].rindex('/') + 1:]} ILLUSTRATION_ID')
+	print(f'Usage: {argv[0][argv[0].rindex('/') + 1:]} ILLUSTRATION_ID [--out-dir OUTPUT_DIRECTORY]')
 	print('\nILLUSTRATION_ID     Numeric id of the illustration to download. (https://www.pixiv.net/artworks/<Illustration ID>)')
+	print('OUTPUT_DIRECTORY    The directory to write the downloaded images into.')
 	print('\nProviding -- for any option will make it be read from stdin.')
 	print('Specifying an option multiple times will overwrite the preceding occurrences.')
 	print('Not specifying an ILLUSTRATION_ID has the same effect as --.')
@@ -16,16 +18,33 @@ if '--help' in argv:
 	print_help()
 
 illust_id = '--'
+out_dir = './'
 
 for i in range(1, len(argv)):
+	if len(argv[i]) > 2 and argv[i].startswith('--'):
+		continue
+
+	if argv[i - 1] == '--out-dir':
+		out_dir = argv[i]
+		continue
+
 	illust_id = argv[i]
 
 if illust_id == '--':
 	illust_id = input('Illustration ID (/artworks/<Illustration ID>): ')
 
+if out_dir == '--':
+	out_dir = input('Output directory: ')
+
 if illust_id == None or not illust_id.isnumeric():
 	print('Invalid Illustration ID.\n')
 	print_help()
+
+if not out_dir.endswith('/'):
+	out_dir += '/'
+
+if not isdir(out_dir):
+	print(f'"{out_dir}" is not a directory.')
 
 url = f'https://www.pixiv.net/ajax/illust/{illust_id}?lang=en'
 
@@ -52,9 +71,9 @@ page_count = illust_info.get('pageCount')
 fileType = orig_url[orig_url.rindex('.')+1:]
 
 for i in range(0, page_count):
-	file_name = f'{illust_id}-{i}.{fileType}'
-	if isfile(file_name):
-		print(f'File "{file_name}" already exitst.')
+	file_path = f'{out_dir}{illust_id}-{i}.{fileType}'
+	if isfile(file_path):
+		print(f'File "{file_path}" already exitst.')
 		continue
 
 	url = orig_url.replace('_p0.', '_p' + f'{i}' + '.')
@@ -62,10 +81,13 @@ for i in range(0, page_count):
 	if img_result.status_code != 200:
 		print(f'Got Status Code {img_result.status_code} when downloading image {i+1} of {page_count}')
 		continue
-
-	with open(file_name, 'wb') as file:
-		file.write(img_result.content)
-		file.close()
-		print(f'Saved image {i+1} of {page_count} to "{file_name}".')
+	
+	try:
+		with open(file_path, 'wb') as file:
+			file.write(img_result.content)
+			file.close()
+			print(f'Saved image {i+1} of {page_count} to "{file_path}".')
+	except:
+		print(f'Failed to write image {i} of {page_count} to "{file_path}".')
 
 print('Done.')
